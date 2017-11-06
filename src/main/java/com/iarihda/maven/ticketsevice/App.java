3877571f15cb46e74a6d78299c156c963d99ed21
Adhirai Manickam <adhirai.manickam@gmail.com>
2017-11-06 16:46:09 -0500

    Initial Run
    Test Hold functionality

--- src/main/java/com/iarihda/maven/ticketsevice/App.java
+++ src/main/java/com/iarihda/maven/ticketsevice/App.java
@@ -1,13 +1,30 @@
-package com.iarihda.maven.ticketsevice;
+import model.SeatHold;
+import service.BookingService;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+
+public class App {
+
+	public static void main(String[] args) {
+		BookingService bs = new BookingService();
+		SeatHold s1 = bs.findAndHoldSeats(4, "email");
+		for(String s : s1.getHeldSeats())
+			System.out.print(s+" ");
+		System.out.println();
+		SeatHold s2 = bs.findAndHoldSeats(5, "email");
+		for(String s : s2.getHeldSeats())
+			System.out.print(s+" ");
+		System.out.println();
+		SeatHold s3 = bs.findAndHoldSeats(2, "email");
+		for(String s : s3.getHeldSeats())
+			System.out.print(s+" ");
+		System.out.println();
+		SeatHold s4 = bs.findAndHoldSeats(4, "email");
+		for(String s : s4.getHeldSeats())
+			System.out.print(s+" ");
+		System.out.println();
+		SeatHold s5 = bs.findAndHoldSeats(6, "email");
+		for(String s : s5.getHeldSeats())
+			System.out.print(s+" ");
+	}
+
 }
