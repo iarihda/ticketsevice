62620105ddcfdb6b53e4adb6231e2b35e2c85357
Adhirai Manickam <adhirai.manickam@gmail.com>
2017-11-07 16:43:28 -0500

    Added threads

--- src/main/java/com/iarihda/maven/ticketsevice/App.java
+++ src/main/java/com/iarihda/maven/ticketsevice/App.java
@@ -1,32 +1,75 @@
-package com.iarihda.maven.ticketsevice;
+package setup;
 
-import com.iarihda.maven.ticketsevice.model.SeatHold;
-import com.iarihda.maven.ticketsevice.service.BookingService;
+import model.SeatHold;
+import service.BookingService;
+
+class TicketThread extends Thread{
+	
+	private BookingService bookingService;
+	private long waitTime;
+	private int numSeats;
+	private String customerEmail;
+	
+	public TicketThread(BookingService bs, long ms, int seatNo, String email) {
+		bookingService = bs;
+		waitTime = ms;
+		numSeats = seatNo;
+		customerEmail = email;
+	}
+	
+	public void run(){
+		try {
+			System.out.println("Starting booking for "+customerEmail);
+			SeatHold sh = bookingService.findAndHoldSeats(numSeats, customerEmail);
+			if(sh==null){
+				throw new InterruptedException("Requested number of seats ("+numSeats+") not available for "+customerEmail+". Please try with lesser quantity.");
+			}
+			Thread.sleep(waitTime);
+			String confirmation = bookingService.reserveSeats(sh.getId(), customerEmail);
+			if(confirmation == null){
+				System.out.println("Hold expired due to time out for "+customerEmail+". Please try again.");
+			} else {
+				System.out.println("Tickets booked for "+customerEmail+". Confirmation Code - "+confirmation);
+				for(String s : sh.getHeldSeats())
+					System.out.print(s+" ");
+				System.out.println();
+			}
+		} catch (InterruptedException e) {
+			System.out.println(e.getMessage());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+}
 
 public class App {
 
 	public static void main(String[] args) {
 		TheatreSetup venue = new TheatreSetup();
-		BookingService bs = new BookingService(venue);
-		SeatHold s1 = bs.findAndHoldSeats(4, "email");
-		for(String s : s1.getHeldSeats())
-			System.out.print(s+" ");
-		System.out.println();
-		SeatHold s2 = bs.findAndHoldSeats(5, "email");
-		for(String s : s2.getHeldSeats())
-			System.out.print(s+" ");
-		System.out.println();
-		SeatHold s3 = bs.findAndHoldSeats(2, "email");
-		for(String s : s3.getHeldSeats())
-			System.out.print(s+" ");
-		System.out.println();
-		SeatHold s4 = bs.findAndHoldSeats(4, "email");
-		for(String s : s4.getHeldSeats())
-			System.out.print(s+" ");
-		System.out.println();
-		SeatHold s5 = bs.findAndHoldSeats(6, "email");
-		for(String s : s5.getHeldSeats())
-			System.out.print(s+" ");
+		BookingService bookingService = new BookingService(venue);
+		try {
+			TicketThread t1 = new TicketThread(bookingService, 15000, 4, "a@gmail.com");
+			t1.start();
+			TicketThread t2 = new TicketThread(bookingService, 0, 2, "b@gmail.com");
+			t2.start();
+			TicketThread t3 = new TicketThread(bookingService, 30000, 5, "c@gmail.com");
+			t3.start();
+			Thread.sleep(5000);
+			TicketThread t4 = new TicketThread(bookingService, 5000, 18, "d@gmail.com");
+			t4.start();
+			Thread.sleep(5000);
+			TicketThread t5 = new TicketThread(bookingService, 5000, 13, "d@gmail.com");
+			t5.start();
+			Thread.sleep(30000);
+			TicketThread t6 = new TicketThread(bookingService, 0, 5, "c@gmail.com");
+			t6.start();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		
+		
 	}
+
 
 }
