fc805904c4e84fa7a231b567ba8415b5536317f0
Adhirai Manickam <adhirai.manickam@gmail.com>
2017-11-07 16:42:42 -0500

    Discard expired holds and reserve tickets

--- src/main/java/com/iarihda/maven/ticketsevice/Service/BookingService.java
+++ src/main/java/com/iarihda/maven/ticketsevice/Service/BookingService.java
@@ -1,58 +1,107 @@
 package service;
 
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map.Entry;
 
-import model.Movie;
-import model.Screen;
+import model.Bookings;
 import model.SeatHold;
 import model.Seats;
-import model.Show;
+import setup.TheatreSetup;
 
+/**
+ * Handles the ticket blocking and booking for a particular show
+ * @author Adhirai Manickam
+ */
 public class BookingService implements TicketService {
 	
-	final int rows = 5;
-	final int columns = 5;
+	private Seats seats;
+	private boolean[][] seatArray; //Seating Array (is true if the seat is held / reserved)
+	private int[] rowAvailability; //No. of seats available in each row
+	private int rows;
+	private int columns;
 	
-	Screen screen;
-	Movie movie;
-	Show show;
-	Seats seats;
-	boolean[][] seatArray;
-	int[] rowAvailability;
-	
-	HashMap<Integer,SeatHold> seatHoldMap;
+	private HashMap<Integer,SeatHold> seatHoldMap; //Maps the seatHoldId with the SeatHold object.
 
-	public BookingService() {
-		screen = new Screen(rows*columns,50,true);
-		movie = new Movie("Avenger", "English", true);
-		show = new Show(movie.getId(),screen.getId(),"6PM",true);
-		seats = new Seats(show.getId(),screen.getCapacity(),rows,columns);
+	/**
+	 * Constructor
+	 * @param venue - A TheatreSetup object with movie, screen and show information
+	 */
+	public BookingService(TheatreSetup venue) {
+		rows = venue.getRowCount();
+		columns = venue.getColumnCount();
+		seats = new Seats(venue.getShow().getId(),venue.getScreen().getCapacity(),rows,columns);
 		seatArray = seats.getSeats();
 		rowAvailability = seats.getRowCount();
 		seatHoldMap = new HashMap<Integer, SeatHold>();
 	}
 	
-	@Override
-	public int numSeatsAvailable() {
+	/**
+	* Discards the expired holds and returns the updated number of seats that are neither held nor reserved
+	* @return the number of tickets available in the venue
+	*/
+	public synchronized int numSeatsAvailable() {
+		removeExpiredHolds();
 		return seats.getAvailabileSeats();
 	}
 
-	@Override
-	public SeatHold findAndHoldSeats(int numSeats, String customerEmail) {
+	/**
+	 * Removes the SeatHold objects which has expired and updates the count of seats available
+	 */
+	private void removeExpiredHolds() {
+		Iterator<Entry<Integer, SeatHold>> it = seatHoldMap.entrySet().iterator();
+		while(it.hasNext()){
+			Entry<Integer, SeatHold> hold = it.next();
+			if(!hold.getValue().isActive()){
+				removeHoldOnSeats(hold.getValue().getHeldSeats());
+				seats.incrementSeats(hold.getValue().getSeatCount());
+				it.remove();
+			}
+		}
+	}
+
+	/**
+	 * Change the availability of the status to 'true' in the seating array
+	 * Update the no. of free seats on that particular row
+	 * @param heldSeats
+	 */
+	private void removeHoldOnSeats(String[] heldSeats) {
+		for(String heldSeat : heldSeats){
+			int r = (int)heldSeat.charAt(0)-65;
+			int c = Character.getNumericValue(heldSeat.charAt(1)) - 1;
+			seatArray[r][c] = false;
+			rowAvailability[r]++;
+		}
+	}
+
+	/**
+	* Find and hold the best available seats for a customer
+	* @param numSeats the number of seats to find and hold
+	* @param customerEmail unique identifier for the customer
+	* @return a SeatHold object identifying the specific seats and related information
+	*/
+	public synchronized SeatHold findAndHoldSeats(int numSeats, String customerEmail) {
 		if(numSeats>numSeatsAvailable())
 			return null;
 		String[] seatsToBeHeld = findBestSeats(numSeats);
 		seats.updateSeatAvailability(seatArray,rowAvailability);
 		SeatHold seatHold = new SeatHold(numSeats, customerEmail, seatsToBeHeld);
 		seatHoldMap.put(seatHold.getId(), seatHold);
+		seats.decrementSeats(numSeats);
 		return seatHold; 
 	}
 
+	/**
+	 * Find continuous seats in one row if available
+	 * Else split up and find the seats based on availability in each row
+	 * @param numSeats
+	 * @return array of seat numbers
+	 */
 	private String[] findBestSeats(int numSeats) {
 		String[] seatNumbers = null;
 		for(int i=rows-1;i>=0;i--){
 			if(rowAvailability[i]>=numSeats) {
-				seatNumbers = reserveSeatsInRow(i, numSeats);
+				seatNumbers = holdSeatsInRow(i, numSeats);
 				break;
 			}
 		}
@@ -62,6 +111,11 @@ public class BookingService implements TicketService {
 		return seatNumbers;
 	}
 
+	/**
+	 * Split up and find the seats based on availability in each row
+	 * @param numSeats
+	 * @return
+	 */
 	private String[] splitAndReserveSeats(int numSeats) {
 		String[] seatNumbers = new String[numSeats];
 		int itr = 0;
@@ -78,7 +132,13 @@ public class BookingService implements TicketService {
 		return seatNumbers;
 	}
 
-	private String[] reserveSeatsInRow(int r, int numSeats) {
+	/**
+	 * Hold the specified no. of vacant seats in one row
+	 * @param r - row number
+	 * @param numSeats - no. of seats to be held
+	 * @return array of seat numbers
+	 */
+	private String[] holdSeatsInRow(int r, int numSeats) {
 		String[] seatNumbers = new String[numSeats];
 		int startIndex = -1;
 		int count = 0;
@@ -100,10 +160,19 @@ public class BookingService implements TicketService {
 		return seatNumbers;
 	}
 
-	@Override
-	public String reserveSeats(int seatHoldId, String customerEmail) {
-		// TODO Auto-generated method stub
-		return null;
+	/**
+	* Commit seats held for a specific customer
+	* @param seatHoldId the seat hold identifier
+	* @param customerEmail the email address of the customer to which the seat hold is assigned
+	* @return a reservation confirmation code
+	*/
+	public synchronized String reserveSeats(int seatHoldId, String customerEmail) {
+		SeatHold seatsToBeBooked = seatHoldMap.get(seatHoldId);
+		if(!seatsToBeBooked.isActive())
+			return null;
+		seatHoldMap.remove(seatHoldId);
+		Bookings newBooking = new Bookings(seatsToBeBooked, customerEmail, seats.getShowId());
+		return newBooking.getConfirmationCode();
 	}
 
 }
