8a1393b91587117a48593ad19c41dded22c7ed15
Adhirai Manickam <adhirai.manickam@gmail.com>
2017-11-07 16:41:36 -0500

    Added 'Bookings' entity
    To store and maintain all confirmed bookings

---
+++ src/main/java/com/iarihda/maven/ticketsevice/model/Bookings.java
@@ -0,0 +1,36 @@
+package model;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class Bookings {
+	
+	SeatHold seats;
+	String customerEmail;
+	String confirmationCode;
+	
+	private static AtomicInteger idGenerator = new AtomicInteger(1000);
+	
+	/**
+	 * Constructor
+	 * @param seatsReserved
+	 * @param customer
+	 * @param movieName
+	 */
+	public Bookings(SeatHold seatsReserved, String customer, int showId) {
+		seats = seatsReserved;
+		customerEmail = customer;
+		confirmationCode = String.valueOf(showId)+String.valueOf(idGenerator.getAndIncrement());
+	}
+	
+	public SeatHold getSeats(){
+		return seats;
+	}
+	
+	public String getCustomer(){
+		return customerEmail;
+	}
+	
+	public String getConfirmationCode(){
+		return confirmationCode;
+	}
+}
