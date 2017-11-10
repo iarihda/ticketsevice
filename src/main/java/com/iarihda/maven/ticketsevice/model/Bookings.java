7a03c157de5d99d0bba886e77ae9ce5c7079bdae
Adhirai Manickam <adhirai.manickam@gmail.com>
2017-11-10 10:49:46 -0500

    Added comments

--- src/main/java/com/iarihda/maven/ticketsevice/model/Bookings.java
+++ src/main/java/com/iarihda/maven/ticketsevice/model/Bookings.java
@@ -2,6 +2,13 @@ package com.iarihda.maven.ticketsevice.model;
 
 import java.util.concurrent.atomic.AtomicInteger;
 
+/**
+ * A domain class which represents a booking for a show. It has the following fields
+ * SeatHold object – The specific seats which are reserved for the booking and related info
+ * Customer Email – Unique Identifier for a customer
+ * Confirmation Code – A unique string which confirms and identifies the booking
+ * @author Adhirai Manickam
+ */
 public class Bookings {
 	
 	SeatHold seats;
@@ -22,14 +29,23 @@ public class Bookings {
 		confirmationCode = String.valueOf(showId)+String.valueOf(idGenerator.getAndIncrement());
 	}
 	
+	/*
+	 * @return seathold object
+	 */
 	public SeatHold getSeats(){
 		return seats;
 	}
 	
+	/*
+	 * @return customer email
+	 */
 	public String getCustomer(){
 		return customerEmail;
 	}
 	
+	/*
+	 * @return confirmationCode
+	 */
 	public String getConfirmationCode(){
 		return confirmationCode;
 	}
