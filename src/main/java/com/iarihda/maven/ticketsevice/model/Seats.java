f484d1aec8ba6ace1672f71c7cb23957cfc8e64c
Adhirai Manickam <adhirai.manickam@gmail.com>
2017-11-10 11:05:47 -0500

    Added comments

--- src/main/java/com/iarihda/maven/ticketsevice/model/Seats.java
+++ src/main/java/com/iarihda/maven/ticketsevice/model/Seats.java
@@ -1,7 +1,11 @@
 package com.iarihda.maven.ticketsevice.model;
 
 /**
- * Transaction entity to maintain and manage the seat availability of a show
+ * An entity which maintains and manages the seats in a show. It contains the following
+ * Show Id – The id of the show for which the seats are managed
+ * Seat Availability – The current number of vacant seats which are neither held not reserved by a customer
+ * Seat Array – Array representation of the seating
+ * Row Availability – No. of seats which are vacant in each row
  * @author Adhirai Manickam
  */
 public class Seats {
@@ -13,10 +17,10 @@ public class Seats {
 	
 	/**
 	 * Constructor
-	 * @param show - Id of the show for which the seats are maintained
-	 * @param capacity - initial available seats is the capacity of the screen
-	 * @param rows - no. of rows
-	 * @param columns - no. of columns
+	 * @param show
+	 * @param capacity
+	 * @param rows
+	 * @param columns
 	 * @throws IllegalArgumentException 
 	 */
 	public Seats(Show s, int capacity, int rows, int columns) throws IllegalArgumentException{
@@ -29,6 +33,10 @@ public class Seats {
 			rowCount[i] = columns;
 	}
 	
+	/**
+	 * Validation to check if the show is not null and active
+	 * @@throws IllegalArgumentException
+	 */
 	private void validateShow(Show s) throws IllegalArgumentException {
 		if(s==null || !s.isActive()){
 			throw new IllegalArgumentException("Show should be active");
@@ -36,45 +44,70 @@ public class Seats {
 		
 	}
 
+	/**
+	 * @return show
+	 */
 	public Show getShow() {
 		return show;
 	}
 	
+	/**
+	 * @return no. of seats available
+	 */
 	public int getAvailabileSeats() {
 		return availableSeats;
 	}
 	
+	
+	/**
+	 * @return seat array
+	 */
 	public boolean[][] getSeats(){
 		return seatArray;
 	}
 	
+	
+	/**
+	 * @return count of vacant seats per row
+	 */
 	public int[] getRowCount(){
 		return rowCount;
 	}
 	
-	void setShow(Show s) {
+	
+	/**
+	 * Change the show after the object is created
+	 * @params show
+	 */
+	public void setShow(Show s) {
+		validateShow(s);
 		show = s;
 	}
 
+	/**
+	 * Increment the no. of availabe seats
+	 * @params new count
+	 */
 	public void incrementSeats(int numSeats) {
 		availableSeats += numSeats;		
 	}
 	
+	
+	/**
+	 * Decrement the no. of availabe seats
+	 * @params new count
+	 */
 	public void decrementSeats(int numSeats) {
 		availableSeats -= numSeats;		
 	}
 	
+	
+	/**
+	 * Update the seat array and row count array
+	 * @params new arrays
+	 */
 	public void updateSeatAvailability(boolean[][] newSeatArr, int[] newRowCount) {
 		seatArray = newSeatArr;
 		rowCount = newRowCount;
-//		printSeats();
-	}
-	
-	private void printSeats(){
-		for(int i=0;i<seatArray.length;i++){
-			for(int j=0;j<seatArray[i].length;j++)
-				System.out.print(seatArray[i][j]+" ");
-			System.out.println();
-		}
 	}
 }
